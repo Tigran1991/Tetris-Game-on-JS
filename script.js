3583553059457c5a954fa1380e6fb6f30ecdd024
Tigran Aginyan <taginyan91@gmail.comm>
2023-04-08 16:01:45 +0400

    creating Tetris game using HTML, CSS and JS

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const container = document.querySelector(".container");
-const canvas = document.getElementById("myCanvas");
 
-const makeGame = () => {
-    const context = canvas.getContext("2d");
-    context.fillStyle = "red";
-};
+const makeGame = () => {};
 
 makeGame();
